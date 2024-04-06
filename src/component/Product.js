19572ba35697117654105521873c67e2bdfb4d86
somesh <someshgupta.lbsim@gmail.com>
2024-04-06 12:37:22 +0530

    commit changes

--- src/component/Product.js
+++ src/component/Product.js
@@ -12,6 +12,8 @@ function Product() {
     const [products,setProduct] = useState([]);
     const [cartItems, setCartItems] = useState([]);
     const [showModal, setShowModal] = useState(false);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [itemsPerPage] = useState(8);
     const [selectedProduct, setSelectedProduct] = useState(null);
     const dispatch = useDispatch();
     const navigate = useNavigate();
@@ -41,28 +43,35 @@ function Product() {
         setShowModal(false);
     };
 
-    const cart = products.map(product => (
-        <div className='col-md-3'>
-                <Card key={product.id} style={{ width: '18rem',height: '22rem',textAlign: 'center',margin: '1rem' }}>
-                    <div className='text-center' style={{'marginTop':'1rem'}}>
-                        <Card.Img variant="top" src={product.thumbnail} style={{ width: '15rem',height: '12rem' }}/>
-                    </div>
-                    <Card.Body>
-                        <Card.Title>{product.title.length > 15 ? product.title.slice(0, 15) + '...' : product.title}</Card.Title>
-                        <Card.Text>
-                                Rs {product.price}
-                        </Card.Text>
-                        {cartItems.includes(product.id) ? (
+    const indexOfLastItem = currentPage * itemsPerPage;
+    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+    const currentProducts = products.slice(indexOfFirstItem, indexOfLastItem);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
+    const cart = currentProducts.map(product => (
+        <div className='col-md-3' key={product.id}>
+            <Card style={{ width: '18rem', height: '22rem', textAlign: 'center', margin: '1rem' }}>
+                <div className='text-center' style={{ marginTop: '1rem' }}>
+                    <Card.Img variant="top" src={product.thumbnail} style={{ width: '15rem', height: '12rem' }} />
+                </div>
+                <Card.Body>
+                    <Card.Title>{product.title.length > 15 ? product.title.slice(0, 15) + '...' : product.title}</Card.Title>
+                    <Card.Text>
+                        Rs {product.price}
+                    </Card.Text>
+                    {cartItems.includes(product.id) ? (
                         <Button variant="secondary" onClick={goToCart}>Go to Cart</Button>
-                    ) : ( 
-                        <>
-                            <Button variant="primary" onClick={() => addToCart(product)}>Add to Cart</Button>
-                            <Button style={{'backgroundColor':'#003C43','color':'#fff','borderColor':'#003C43','marginLeft':'1rem'}} onClick={() => handleViewDetails(product)}>View Details</Button>
-                        </>                    )}
+                    ) : (
+                            <>
+                                <Button variant="primary" onClick={() => addToCart(product)}>Add to Cart</Button>
+                                <Button style={{ 'backgroundColor': '#003C43', 'color': '#fff', 'borderColor': '#003C43', 'marginLeft': '1rem' }} onClick={() => handleViewDetails(product)}>View Details</Button>
+                            </>
+                        )}
                 </Card.Body>
-            </Card>   
+            </Card>
         </div>
-    ))
+    ));
 
     const modalContent = selectedProduct && (
         <Modal show={showModal} onHide={handleCloseModal}>
@@ -70,20 +79,19 @@ function Product() {
                 <Modal.Title>{selectedProduct.title}</Modal.Title>
             </Modal.Header>
             <Modal.Body>
-            <Carousel interval={1000} pause={false}>
-                {selectedProduct.images.map((image, index) => (
-                    <Carousel.Item key={index}>
-                        <img
-                            className="d-block w-100"
-                            src={image}
-                            style={{ width: '15rem',height: '12rem' }}
-                            alt={`Slide ${index}`}
-                        />
-                    </Carousel.Item>
-                ))}
-            </Carousel>
+                <Carousel interval={1000} pause={false}>
+                    {selectedProduct.images.map((image, index) => (
+                        <Carousel.Item key={index}>
+                            <img
+                                className="d-block w-100"
+                                src={image}
+                                style={{ width: '15rem', height: '12rem' }}
+                                alt={`Slide ${index}`}
+                            />
+                        </Carousel.Item>
+                    ))}
+                </Carousel>
                 <div>
-                    {/* <img src={selectedProduct.thumbnail} style={{ width: '15rem',height: '12rem' }}/> */}
                     <p>Price: Rs {selectedProduct.price}</p>
                     <p>{selectedProduct.description}</p>
                 </div>
@@ -96,17 +104,34 @@ function Product() {
         </Modal>
     );
 
-  return (
-    <>
-        <Container>
-            <div className='row'>
-                {cart}
-            </div>
-        </Container>
-        {modalContent}
-        
+    // Logic for rendering pagination buttons
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(products.length / itemsPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <>
+            <Container>
+                <div className='row'>
+                    {cart}
+                </div>
+                <nav>
+                    <ul className='pagination'>
+                        {pageNumbers.map(number => (
+                            <li key={number} className='page-item'>
+                                <button onClick={() => paginate(number)} className='page-link'>
+                                    {number}
+                                </button>
+                            </li>
+                        ))}
+                    </ul>
+                </nav>
+            </Container>
+            {modalContent}
+
         </>
     )
 }
 
-export default Product
+export default Product;
